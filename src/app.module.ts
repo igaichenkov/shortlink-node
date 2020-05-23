061efb49856baaccb1215f8ebb6012c030a8d2d5
Igor Gaichenkov <i.gaichenkov@outlook.com>
2020-05-23 17:37:18 +0200

    Project cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LinksModule } from './links/links.module';
 
 @Module({
   imports: [LinksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
