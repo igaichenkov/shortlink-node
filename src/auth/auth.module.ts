560b1c36625d6464de804d3283197f749ddc040c
Igor Gaichenkov <i.gaichenkov@outlook.com>
2021-06-08 22:55:45 +0200

    Fixed packages' volnurabilities

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ApiKeyStrategy } from './apiKey.strategy';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
     imports: [PassportModule, UsersModule],
